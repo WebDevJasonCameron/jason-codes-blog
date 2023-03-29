b4cc2209e7ef1931aa8d30ac9ae36fc68387707d
Jason Cameron <dev.jasoncameron@gmail.com>
2023-03-28 20:04:31 -0500

    adding html and css to start

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<main>
+			<header>
+				<a href='' className='logo'>
+					Jason Codes Blog
+				</a>
+				<nav>
+					<a href=''>Login</a>
+					<a href=''>Register</a>
+				</nav>
+			</header>
+			<div className='post'>
+				<div className='image'>
+					<img src='https://techcrunch.com/wp-content/uploads/2023/03/Screenshot-2023-03-27-at-5.06.44-PM.jpg?w=1390&crop=1' />
+				</div>
+				<div className='texts'>
+					<h2>All the tech and features in the all-electric Kia EV9 SUV</h2>
+					<p className='info'>
+						<span className='author'>Jason Cameron</span>
+						<time>2023-03-28 16:45</time>
+					</p>
+					<p>
+						Kia revealed Tuesday the next EV in its portfolio, a three-row
+						flagship SUV called the EV9 that is packed with tech and a key
+						product in the company’s mission to reach annual sales of 1.2
+						million battery electric vehicles by 2030.
+					</p>
+				</div>
+			</div>
+			<div className='post'>
+				<div className='image'>
+					<img src='https://techcrunch.com/wp-content/uploads/2023/03/Screenshot-2023-03-27-at-5.06.44-PM.jpg?w=1390&crop=1' />
+				</div>
+				<div className='texts'>
+					<h2>All the tech and features in the all-electric Kia EV9 SUV</h2>
+					<p>
+						Kia revealed Tuesday the next EV in its portfolio, a three-row
+						flagship SUV called the EV9 that is packed with tech and a key
+						product in the company’s mission to reach annual sales of 1.2
+						million battery electric vehicles by 2030.
+					</p>
+				</div>
+			</div>
+			<div className='post'>
+				<div className='image'>
+					<img src='https://techcrunch.com/wp-content/uploads/2023/03/Screenshot-2023-03-27-at-5.06.44-PM.jpg?w=1390&crop=1' />
+				</div>
+				<div className='texts'>
+					<h2>All the tech and features in the all-electric Kia EV9 SUV</h2>
+					<p>
+						Kia revealed Tuesday the next EV in its portfolio, a three-row
+						flagship SUV called the EV9 that is packed with tech and a key
+						product in the company’s mission to reach annual sales of 1.2
+						million battery electric vehicles by 2030.
+					</p>
+				</div>
+			</div>
+		</main>
+	);
 }
 
 export default App;
