742bbe2027da7dc2d063f19edc240f5ab8c3996c
Jason Cameron <dev.jasoncameron@gmail.com>
2024-01-23 19:53:25 -0600

    change get to GET for the rss feed.  not what the insturctor has, but this is IMPORTANT!

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,22 @@
+import rss from '@astrojs/rss';
+
+import { formatBlogPosts } from "../js/utils.js"
+
+const postImportResult = import.meta.glob('./**/*.md', { eager: true });
+const posts = formatBlogPosts(Object.values(postImportResult));
+
+export const GET = () => rss({
+    stylesheet: '/rss/styles.xsl',
+    title: 'My Astro Blog',
+    description: 'A humble Astronaut’s guide to the stars',
+    site: import.meta.env.SITE,
+    items: posts.map((post) => ({
+        link: post.url,
+        title: post.frontmatter.title,
+        pubDate: post.frontmatter.date,
+        description: post.frontmatter.description,
+        customData: `
+      <author>${post.frontmatter.author}</author>
+    `
+    }))
+});
