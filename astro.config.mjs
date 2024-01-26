8677e7f42d340cb47e364374e42f02bff46b5b69
Jason Cameron <dev.jasoncameron@gmail.com>
2024-01-25 18:46:47 -0600

    adding react lib to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import sitemap from "@astrojs/sitemap";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  site:'https://astro-blog-cip.netlify.app',
-  integrations: [icon(), sitemap()]
+  site: 'https://astro-blog-cip.netlify.app',
+  integrations: [icon(), sitemap(), react()]
 });
