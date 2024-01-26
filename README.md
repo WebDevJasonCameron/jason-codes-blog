8ed0d9d395db2f8a438ee52568766956fd96ce0a
Jason Cameron <dev.jasoncameron@gmail.com>
2024-01-25 19:01:56 -0600

    added tailwind!

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ Tech used in this blog...
 2. Astro Icons (See integrations)
 3. astrojs/image
 4. PostCSS
+
+Some Commands to remember...
+- npx astro add react
+- npx astro add tailwind
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import sitemap from "@astrojs/sitemap";
-
 import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro-blog-cip.netlify.app',
-  integrations: [icon(), sitemap(), react()]
+  integrations: [icon(), sitemap(), react(), tailwind()]
 });
